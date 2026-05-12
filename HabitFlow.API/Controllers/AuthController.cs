540eccdcbaba09016546ff2b204cadabbee5e246
Blaxflow <ebesaxizab093@gmail.com>
2026-05-12 10:42:22 +0300

    Initial commit - JWT auth, BCrypt, EF Core

---
+++ HabitFlow.API/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+﻿using HabitFlow.API.DTOs;
+using HabitFlow.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+
+namespace HabitFlow.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+        public AuthController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username == dto.Username))
+            {
+                return BadRequest("Пользователь уже существует");
+            }
+            var user = new User
+            {
+                Username = dto.Username,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+            return Ok("Регистрация успешна");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto) {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == dto.Username);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash))
+            {
+                return Unauthorized("Неверный логин или пароль");
+                
+            }
+            var token = GenerateToken(user);
+            return Ok(new { token });
+        }
+
+
+        
+
+        private string GenerateToken(User user)
+        {
+            var claims = new[]
+    {
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        new Claim(ClaimTypes.Name, user.Username)
+    };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(24),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ HabitFlow.API/DTOs/AuthDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HabitFlow.API.DTOs
+{
+    
+        public class LoginDto
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+        public class RegisterDto
+        {
+            [Required]
+            [MinLength(3, ErrorMessage ="Минимум 3 символа")]
+            public string Username { get; set; }
+            [Required]
+            [EmailAddress(ErrorMessage ="Неверный формат email")]
+            public string Email { get; set; }
+            [Required]
+            [MinLength(6, ErrorMessage = "Минимум 6 символов")]
+            public string Password { get; set; }
+        }
+    
+}
